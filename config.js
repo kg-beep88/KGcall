8ddad06ca7c8d9eeb595324a35a60568918d05f4
kg-beep88 <kgchesterlee@gmail.com>
2026-01-05 15:56:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.APP_CONFIG = {
-  CLIENT_ID: "311576914016-t75jtnmu1k14o0mmarr0jhu49em9nb71.apps.googleusercontent.com",
+  CLIENT_ID: "695871721774-1e7q94tl17ekbquihm3f5uq37r8k5hu8.apps.googleusercontent.com",
   DEFAULT_FILTER: "#KG",               // tag stored in DESCRIPTION for filtering
   DEFAULT_DURATION_MIN: 120,
   CALENDAR_ID: "primary",              // default calendar; you can choose in app
