61db527f955249d3a87842515236db6eca3806e2
kg-beep88 <kgchesterlee@gmail.com>
2026-01-08 16:22:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ window.APP_CONFIG = {
   CALENDAR_ID: "primary",              // default calendar; you can choose in app
 
   // EDIT THESE LISTS (for dropdowns)
-  FOREMEN: ["阿牛", "Ah Hock", "阿坤","阿通", "阿辉", "阿蔡", "阿赖",
-  "阿耀", "阿权", "阿明", "阿文","阿志", "阿平", "阿川", "阿华","阿兴", "阿以", "阿亿",
-  "阿Siong","阿仁", "阿LIM", "阿Kong", "阿Kiong", "阿DI", "蜡笔", "老虎", "忠发", "小生",
+  FOREMEN: ["阿牛", "Ah Hock", "阿坤", "阿通", "阿辉", "阿蔡", "阿赖",
+  "阿耀", "阿权", "阿明", "阿文","阿志", "阿平", "阿川", "阿华", "阿兴", "阿以", "阿亿",
+  "阿Siong", "阿仁", "阿LIM", "阿Kong", "阿Kiong", "阿DI", "蜡笔", "老虎", "忠发", "小生",
   "小牛", "子锋", "伟杰", "仔仔", "Ivan", "Ebi", "Ah Ngei", "阿生", "阿汉", "阿富", "阿宝", "阿伟",
    "阿仔", "阿Yap", "阿Mun", "阿Fook", "阿Cheong", "老国", "益发", "小黑", "小春", "小勇", "小光",
     "Lulu", "Eddie", "阿轩", "阿添", "阿忠", "小盛", "小富", "小伟", "Faizul"
